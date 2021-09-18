fa66ecdbd1cc18560d9a8e617183ec2eaccc4f14
Gustavo Higuti de Souza <g250355@dac.unicamp.br>
2021-09-18 14:03:49 -0300

    Jogo funcionendo v1, precisa de melhorias e correções

---
+++ README.txt
@@ -0,0 +1,10 @@
+# Pong_pygame
+
+## Coisas para melhorar:
+  * Receber direito a velocidade
+  * dar um tempo para a pessoa pensar antes do jogo começar
+  * Arrumar a tela de vitória
+  * Implementar a possibilidade de jogar contra bots
+  * Dar um título
+  * Aleatorizar o lançamento da bola
+
--- v1_Pong.py
+++ v1_Pong.py
@@ -1,2 +1,162 @@
 import pygame
-import keyboard
+from pygame.locals import *
+import random
+import time
+import math
+from sys import exit, maxsize
+from os import system
+
+P_max = 0
+vel_bola = 0
+vel_barra = 0
+
+def start():
+    global P_max,vel_bola,vel_barra
+    system('cls')
+    try:
+        P_max = int(input("Digite a pontuação máxima: "))
+        vel_bola = int(input('Digite a velocidade da bola: '))
+        vel_barra = int(input("Digite a velocidade da barra: "))
+    except:
+        P_max = 5
+        vel_bola = 15
+        vel_barra = 10
+
+start()
+
+#dimensões da tela
+width = 1000
+height = 640
+altura_sup = 100
+altura_inf = 620
+
+#configurações barras
+x1_init = 30
+y1_init = int(height/2)-25
+x1,y1 = x1_init,y1_init
+x2 = width-x1_init-20
+y2 = y1_init
+larg_barra = 20
+alt_barra = 70
+
+#configurações bola
+xb = int(width/2)
+yb = int(height/2)
+b_radius = 10
+velx_bola = random.randint(int(3*vel_bola/7),int(5*vel_bola/7))
+vely_bola = math.sqrt(vel_bola*vel_bola+velx_bola*velx_bola)
+B0 = 0
+
+#pontuação
+j1 = 0
+j2 = 0
+Vencedor = ''
+
+def mov_bola():
+    global xb,yb,velx_bola,vely_bola
+    if yb+b_radius<altura_inf and yb-b_radius>altura_sup:
+        xb += velx_bola
+        yb += vely_bola
+    else:
+        vely_bola = (-1)*vely_bola
+        xb += velx_bola
+        yb += vely_bola
+
+def reset():
+    #configurações barras
+    global x1,y1,x2,y2,xb,yb,velx_bola,vely_bola,B0
+    x1,y1 = x1_init,y1_init
+    x2 = width-x1_init-20
+    y2 = y1_init
+
+    #configurações bola
+    xb = int(width/2)
+    yb = int(height/2)
+    velx_bola = 7.5
+    vely_bola = 10
+    B0 = 0
+
+pygame.init()
+
+def formas():
+    global barra1,barra2,bola
+    barra1 = pygame.draw.rect(scr,(0,255,0),(x1,y1,larg_barra,alt_barra))
+    barra2 = pygame.draw.rect(scr,(255,0,0),(x2,y2,larg_barra,alt_barra))
+    bola = pygame.draw.circle(scr,(255,255,255),(xb,yb),b_radius)
+
+fonte = pygame.font.SysFont('arial',30,bold=True,italic=True)#fonte de texto
+fonteG = pygame.font.SysFont('arial',200,bold=True,italic=True)#fonte de texto
+
+#configurar a tela
+scr = pygame.display.set_mode((width, height))
+pygame.display.set_caption("PONG GAME") #Nome da tela
+relogio = pygame.time.Clock()
+
+time.sleep(2)
+
+#loop principal
+while True:
+    relogio.tick(30) #frames per second
+    scr.fill((10,10,10)) #clear screen
+    mensagem1 = f'Jogador 1: {j1}'
+    mensagem2 = f'Jogador 2: {j2}'
+    texto1 = fonte.render(mensagem1, True, (255,255,255))
+    texto2 = fonte.render(mensagem2, True, (255,255,255))
+
+    #Events loop
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            exit()
+
+    if B0==0:
+        formas()
+        _OO = 0
+        pygame.time.wait(1)
+        velx_bola = velx_bola*random.choice([-1,1])
+        vely_bola = vely_bola*random.choice([-1,1])
+        B0 = 1
+
+    if pygame.key.get_pressed()[K_w]:
+        if y1>altura_sup:
+            y1-=vel_barra
+    if pygame.key.get_pressed()[K_s]:
+        if y1+alt_barra<altura_inf:
+            y1+=vel_barra
+    if pygame.key.get_pressed()[K_UP]:
+        if y2>altura_sup:
+            y2-=vel_barra
+    if pygame.key.get_pressed()[K_DOWN]:
+        if y2+alt_barra<altura_inf:
+            y2+=vel_barra
+
+    if xb<0:
+        j2+=1
+        reset()
+    elif xb>=width:
+        j1+=1
+        reset()
+    if j1>=P_max or j2>=P_max:
+        if j1>j2:
+            Vencedor='Jogador 1 venceu!'
+            mensagemVence = f'{Vencedor}'
+            textoV = fonte.render(mensagemVence, True, (0,255,0))
+        if j2>j1:
+            Vencedor='Jogador 2 venceu!'
+            mensagemVence = f'{Vencedor}'
+            textoV = fonte.render(mensagemVence, True, (255,0,0))
+        scr.blit(textoV, (400, 300))
+
+    limite_cima = pygame.draw.line(scr,(255,255,255),(0,altura_sup),(width,altura_sup))
+    limite_baixo = pygame.draw.line(scr,(255,255,255),(0,altura_inf),(width,altura_inf))
+    formas()
+
+    if bola.colliderect(barra1) or bola.colliderect(barra2):
+        velx_bola = velx_bola*(-1)
+
+    mov_bola()
+
+    scr.blit(texto1, ((width/2)-80, 10))
+    scr.blit(texto2, ((width/2)-80, 40))
+
+    pygame.display.flip()    
