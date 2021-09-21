2b3f8b1cd548d5814e0c9684dba9d58e3d121333
Gustavo Higuti de Souza <g250355@dac.unicamp.br>
2021-09-20 21:50:12 -0300

    POO e Menu
    Programação Orientada a Objeto, interação entre arquivos internos e Menu de seleção implementados

--- menu.py
+++ menu.py
@@ -2,29 +2,36 @@ import pygame
 from pygame.locals import *
 import PySimpleGUI as sg
 import random
+import v3_POO_Pong as game
 
 class Screens():
-    def __init__(self):
+    def __init__(self): 
         layout = [
             [sg.Text('Pontuação final: ')],
-            [sg.Slider(range=(3,10),default_value=2,orientation='h', size=(23,20), key='Max_round')],
+            [sg.Slider(range=(3,10),default_value=5,orientation='h', size=(30,20), key='Max_round')],
             [sg.Text('Velocidade da bola: ')],
-            [sg.Slider(range=(1,4),default_value=8,orientation='h', size=(23,20), key='Vel_bola')],
+            [sg.Slider(range=(1,4),default_value=2,orientation='h', size=(30,20), key='Vel_bola')],
             [sg.Text('Velocidade da barra: ')],
-            [sg.Slider(range=(1,4),default_value=8,orientation='h', size=(23,20), key='Vel_barra')],
+            [sg.Slider(range=(1,4),default_value=2,orientation='h', size=(30,20), key='Vel_barra')],
             [sg.Button('INICIAR')]
         ]
         #Janela
-        self.janela = sg.Window('Gerador de senha').layout(layout)
+        self.janela = sg.Window('PONG MENU').layout(layout)
     def Iniciar(self):   
         global P_max,v_bola,v_barra       
         self.button, self.values = self.janela.read()
-        
-        if self.values!=None:
-            P_max = self.values['Max_round']
-            v_bola = self.values['Vel_bola']
-            v_barra = self.values['Vel_barra']
+        try:
+            if self.values!=None:
+                P_max = self.values['Max_round']
+                v_bola = 8 + self.values['Vel_bola']*2
+                v_barra = 2 + self.values['Vel_barra']*4
+                self.janela.close()
+        except:
+            exit()
 
-tela = Screens()
-tela.Iniciar()
-Jogo = pong()
+while True:
+    tela = Screens()
+    tela.Iniciar()
+    jogo = game.Pong(P_max,v_bola,v_barra)
+    jogo.tela()
+    del jogo
---
+++ v3_POO_Pong.py
@@ -0,0 +1,172 @@
+import pygame
+import random
+import time
+from pygame.locals import *
+
+class Pong:
+    def __init__(self,P_max,V_bola,V_barra):
+        pygame.init()
+        self.largura = 1000
+        self.altura_tela = 620
+        self.altura_sup = 100
+        self.altura_inf = 600
+        self.y_init = 295
+        self.y1 = self.y_init
+        self.y2 = self.y_init
+        self.x1 = 30
+        self.x2 = 950
+        self.xb = 500
+        self.yb = 320
+        self.j1 = 0
+        self.j2 = 0
+        self.B0 = 0
+        self.P_max = P_max
+        self.v_bola = V_bola
+        self.vx_bola = int(self.v_bola*random.randint(6,8)/10)
+        self.vy_bola = V_bola - self.vx_bola
+        self.v_barra = V_barra
+        self.fonte = pygame.font.SysFont('arial',30,bold=True,italic=True)#fonte de texto
+        self.fonteG = pygame.font.SysFont('arial',50,bold=True,italic=True)#fonte de texto
+        self.larg_barra = 20
+        self.alt_barra = 70
+        self.b_radius = 10
+        self.colide1 = time.time()
+
+    def mov_bola(self):
+        if self.yb-10>self.altura_sup and self.yb+10<self.altura_inf:
+            self.xb += self.vx_bola
+            self.yb += self.vy_bola
+        else:
+            self.vy_bola = (-1)*self.vy_bola
+            self.xb += self.vx_bola
+            self.yb += self.vy_bola
+
+    def reset(self):
+        self.y1 = self.y_init
+        self.y2 = self.y_init
+        self.x1 = 30
+        self.x2 = 950
+        self.xb = 500
+        self.yb = 320
+        self.B0 = 0
+        self.vx_bola = int(self.v_bola*random.randint(6,8)/10)
+        self.vy_bola = self.v_bola - self.vx_bola
+        self.v_barra = self.v_barra
+
+    def formas(self):
+        self.limite_cima = pygame.draw.line(self.scr,(255,255,255),(0,self.altura_sup-5),(self.largura,self.altura_sup-5))
+        self.limite_baixo = pygame.draw.line(self.scr,(255,255,255),(0,self.altura_inf+5),(self.largura,self.altura_inf+5))
+        self.limite_centro = pygame.draw.line(self.scr,(255,255,255),(self.largura/2,self.altura_sup-5),(self.largura/2,self.altura_inf+5))
+        self.barra1 = pygame.draw.rect(self.scr,(0,255,0),(self.x1,self.y1,self.larg_barra,self.alt_barra))
+        self.barra2 = pygame.draw.rect(self.scr,(255,0,0),(self.x2,self.y2,self.larg_barra,self.alt_barra))
+        self.bola = pygame.draw.circle(self.scr,(255,255,255),(self.xb,self.yb),self.b_radius)
+
+        self.mensagem1 = f'Jogador 1: {self.j1}'
+        self.mensagem2 = f'Jogador 2: {self.j2}'
+        self.texto1 = self.fonte.render(self.mensagem1, True, (255,255,255))
+        self.texto2 = self.fonte.render(self.mensagem2, True, (255,255,255))
+        self.scr.blit(self.texto1, ((self.largura/2)-80, 10))
+        self.scr.blit(self.texto2, ((self.largura/2)-80, 40))
+    
+    def tela(self):
+        self.scr = pygame.display.set_mode((self.largura, self.altura_tela))
+        pygame.display.set_caption("PONG GAME") #Nome da tela
+        self.relogio = pygame.time.Clock()
+
+        while True:
+            self.relogio.tick(30) #frames per second
+            self.scr.fill((10,10,10)) #clear screen
+            self.mensagem1 = f'Jogador 1: {self.j1}'
+            self.mensagem2 = f'Jogador 2: {self.j2}'
+            self.texto1 = self.fonte.render(self.mensagem1, True, (255,255,255))
+            self.texto2 = self.fonte.render(self.mensagem2, True, (255,255,255))
+
+            #Events loop
+            try:
+                for event in pygame.event.get():
+                    if event.type == QUIT:
+                        pygame.display.quit()
+                        pygame.quit()
+                        #exit()
+                    if event.type == KEYDOWN:
+                        if pygame.key.get_pressed()[K_r]:
+                            self.reset()
+                            self.j1=0
+                            self.j2=0
+
+
+                if pygame.key.get_pressed()[K_w]:
+                    if self.y1>self.altura_sup:
+                        self.y1-=self.v_barra
+                if pygame.key.get_pressed()[K_s]:
+                    if self.y1+self.alt_barra<self.altura_inf:
+                        self.y1+=self.v_barra
+                if pygame.key.get_pressed()[K_UP]:
+                    if self.y2>self.altura_sup:
+                        self.y2-=self.v_barra
+                if pygame.key.get_pressed()[K_DOWN]:
+                    if self.y2+self.alt_barra<self.altura_inf:
+                        self.y2+=self.v_barra
+
+            except:
+                pygame.display.quit()
+                pygame.quit()
+                break
+
+            if self.B0==0:
+                self.vx_bola = self.vx_bola*random.choice([-1,1])
+                self.vy_bola = self.vy_bola*random.choice([-1,1])
+                self.formas()
+                pygame.time.delay(500)
+                self.B0=1
+
+            if self.xb+self.b_radius<0:
+                self.j2+=1
+                self.reset()
+            elif self.xb-self.b_radius>=self.largura:
+                self.j1+=1
+                self.reset()
+            if self.j1>=self.P_max or self.j2>=self.P_max:
+                if self.j1>self.j2:
+                    Vencedor='Jogador 1 venceu!'
+                    mensagemVence = f'{Vencedor}'
+                    textoV = self.fonteG.render(mensagemVence, True, (0,255,0))
+                if self.j2>self.j1:
+                    Vencedor='Jogador 2 venceu!'
+                    mensagemVence = f'{Vencedor}'
+                    textoV = self.fonteG.render(mensagemVence, True, (255,0,0))
+                self.scr.blit(textoV, (300, 500))
+                self.reset()
+                pygame.display.flip() 
+                pygame.time.delay(2000)
+                self.j1 = 0
+                self.j2 = 0
+
+            self.formas()
+
+            if self.bola.colliderect(self.barra1) or self.bola.colliderect(self.barra2):
+                #try:
+                #   self.colide1>3
+                #except:
+                #   self.colide1 = time.time()
+                
+                self.colide2 = time.time()
+                if self.colide2 - self.colide1 > 0.5:
+                    self.vx_bola = self.vx_bola*(-1.1)
+                    _t = 0
+                elif _t==0:
+                    self.vy_bola = (-1.2)*self.vy_bola
+                    self.vx_bola = self.vx_bola*(0.8)
+                    _t = 1
+                self.colide1 = self.colide2
+
+            self.mov_bola()
+
+            self.scr.blit(self.texto1, ((self.largura/2)-80, 10))
+            self.scr.blit(self.texto2, ((self.largura/2)-80, 40))
+
+            pygame.display.flip()
+
+
+#jogo = Pong(3,10,10)
+#jogo.tela()
